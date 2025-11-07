9ab08a5308667ff9a4b91f0ed2abccb88feb732b
Thae Thae Hsu <thaethaehsu23597@gmail.com>
2025-11-07 07:15:35 +0000

    Finished Assignment2

--- ex1.py
+++ ex1.py
@@ -1,3 +1,11 @@
-def find(s, n):
+def find(num, target):
+    for i in range (len(num)):
+        for j in range (i+1,len(num)):
+            if num[i]+num[j]==target:
+                return [i,j]
 # write your implementation here
     return None
+
+print(find([2,7,11,15],9))
+print(find([3,2,4],6))
+print(find([3,3],6))
